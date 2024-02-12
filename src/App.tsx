fd4e4bfe147441e725672c349664fdb1d4feecc5
iovin2001 <100416507+iovin2001@users.noreply.github.com>
2024-02-12 23:17:43 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.less";
+import GitHubButton from "react-github-btn";
+import { Routes } from "./routes";
+import "./main.css";
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
